b9456d537c68c0b4fde9d9b4e7b0650ce9fd84fb
atifkarim <atif0902082@gmail.com>
2019-08-13 23:47:40 +0200

    call function for making datetime dataframe, dataset for training and testing, performing prediction using scikit learn model

--- data_prediction_of_PowerPlant/main.py
+++ data_prediction_of_PowerPlant/main.py
@@ -4,6 +4,12 @@ Created on Sat Aug 10 18:46:22 2019
 
 @author: atif
 """
+import json
+import collections
+import os
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import *
+from sklearn import linear_model
 
 from dataset_analysis import create_dataframe
 from dataset_analysis import conversion_timestamp_to_unixtime
@@ -18,8 +24,15 @@ from dataset_analysis import drop_string_column
 from dataset_analysis import feature_selection_with_selectKbest
 from dataset_analysis import pearson_correlation
 from dataset_analysis import make_dataframe_with_high_correlated_value
+from dataset_analysis import dataframe_date_time_type
+from dataset_analysis import my_sum
+from dataset_analysis import draw_graph
 
-import json
+from model_file import make_dataset
+from model_file import scikit_learn_model
+e = my_sum(9,2)
+print(e)
+
 
 with open('variable_config.json', 'r') as f:
     config = json.load(f)
@@ -30,8 +43,11 @@ end_pos = config['DEFAULT']['end_point_dataframe']
 date_column = config['DEFAULT']['date_column']
 target_column = config['DEFAULT']['target_column']
 furnace_signal_column = config['DEFAULT']['blast_furnace_signal']
-print(type(date_column))
-print(date_column)
+max_best_number = config['DEFAULT']['max_best_number']
+correlation_threshold_min_value = config['DEFAULT']['correlation_threshold_min_value']
+correlation_threshold_max_value = config['DEFAULT']['correlation_threshold_max_value']
+print(type(correlation_threshold_max_value))
+print(correlation_threshold_max_value)
 
 #filepath = 'E:/University of Bremen MSc/masters_thesis/IAT_sebastian/dataset_26_april_3.csv'
 # reading CSV file
@@ -85,7 +101,6 @@ dataframe_datetime = dataframe_no_string.set_index('dateTime')
 
 
 # Feature selection with Sklearn feature best technique
-max_best_number = 20
 sklearn_feature_best_dataframe = feature_selection_with_selectKbest(dataframe_datetime,max_best_number)
 
 # feature selection with Pearson Correlation.
@@ -93,9 +108,6 @@ sklearn_feature_best_dataframe = feature_selection_with_selectKbest(dataframe_da
 sklearn_correlation, main_correlation = pearson_correlation(sklearn_feature_best_dataframe, dataframe_datetime)
 
 # make a dataframe with signal who is lies between a given range of correlation threshold value
-correlation_threshold_value = 0.5
-max_value = 0.9
-
 main_frame = dataframe_datetime
 correlated_frame = main_correlation
 
@@ -103,4 +115,37 @@ correlated_frame = main_correlation
 # correlated_frame = sklearn_correlation
 
 dataframe_high_correlation = make_dataframe_with_high_correlated_value(main_frame,correlated_frame,
-                                                             correlation_threshold_value,max_value)
+                                                             correlation_threshold_min_value, correlation_threshold_max_value)
+
+df = dataframe_date_time_type(dataframe_datetime)
+
+dict_of_dates = {k: v for k, v in df.groupby('Date')}
+dict_of_day_type = {k:v for k,v in df.groupby('TypeofDAY')}
+dict_of_day_name = {k:v for k,v in df.groupby('day_name')}
+
+
+date_key_value = collections.OrderedDict(dict_of_dates)
+day_type_key_value = collections.OrderedDict(dict_of_day_type)
+day_name_key_value = collections.OrderedDict(dict_of_day_name)
+
+current_directory = os.getcwd()
+print(current_directory)
+address = 'image_folder'
+final_directory = current_directory+'/'+str(address)
+if not os.path.exists(final_directory):
+    os.makedirs(final_directory)
+    print('created : ', final_directory)
+else:
+    pass
+
+draw_graph_date = draw_graph(date_key_value,dict_of_dates, target_column,final_directory, subfolder_name = 'date_fig')
+draw_graph_week = draw_graph(day_type_key_value,dict_of_day_type, target_column,final_directory, subfolder_name = 'week_fig')
+draw_graph_day = draw_graph(day_name_key_value,dict_of_day_name, target_column,final_directory, subfolder_name = 'day_fig')
+
+
+train_input, train_output, test_input, test_output = make_dataset(dataframe_high_correlation)
+
+model_list = [LinearRegression(), ExtraTreesRegressor()]
+name = ['linearregression','extraregressor']
+
+model_result = scikit_learn_model(model_list, name, train_input, train_output, test_input, test_output)
