ad3175bf7348d4e74f5e70f8c7de3306d8c3c490
atifkarim <atif0902082@gmail.com>
2019-08-15 10:53:54 +0200

    function for predicting data using sklearn model and plotting the result has added.

--- data_prediction_of_PowerPlant/model_file.py
+++ data_prediction_of_PowerPlant/model_file.py
@@ -31,13 +31,61 @@ def make_dataset(dataframe):
 
     return train_input, train_output, test_input, test_output
 
+
+def plot_graph(test_output, predicted_output):
+    plt.plot((min(test_output), max(test_output)), (min(predicted_output), max(predicted_output)), color='red')
+    plt.scatter(test_output, predicted_output, color='blue')
+    # plt.savefig(model+'_'+'figure_actual_vs_predicted_with_best_fit_line.jpg')
+    plt.xlabel('test_output')
+    plt.ylabel('predicted_output')
+    plt.title('scatter plotting of predicted_output alongside with the average line of test and predicted output')
+    plt.show()
+
+    difference_of_value = predicted_output - test_output
+    print(type(difference_of_value))
+
+    plt.plot(difference_of_value[:])
+    plt.title('observation of the difference of actual and predicted value')
+
+    # plt.rcParams['xtick.labelsize']=2
+    # plt.rcParams['ytick.labelsize']=2
+    # plt.tick_params(labelsize=20)
+    plt.ylabel('difference of value')
+    plt.xlabel('range')
+    plt.grid(b=None, which='both', axis='both')
+    # plt.savefig(model+'_'+'difference_of_actual_and_predicted_value.png')
+    plt.show()
+
+    plt.hist(difference_of_value, bins=20)
+    # plt.xlim(-10,10,1)
+    # plt.savefig(model+'_'+'histogram_of_difference_value.jpg')
+    plt.xlabel('value')
+    plt.ylabel('frequency')
+    plt.title('histogram of value of difference')
+    plt.show()
+
+    plt.plot(predicted_output[0:len(predicted_output[0:])], color='blue')
+    plt.plot(test_output[0:], color='red')
+    # plt.xlim(0,40,1)
+    # plt.ylim(50,70,1)
+    # plt.savefig(model+'_'+'figure_difference_between_actual_and_predicted_value.jpg')
+    plt.xlabel('range')
+    plt.ylabel('value of test and predicted output')
+    plt.title('Visualization of test and predicted output in the same timestamp')
+    plt.show()
+
+
+
 #(model_list: object, name: object, train_input: object, train_output: object, test_input: object, test_output: object) -> object
-def scikit_learn_model(model_list, name, train_input,train_output, test_input, test_output):
+def scikit_learn_model(model_list, name, train_input, train_output, test_input, test_output):
     for idx, i in enumerate(model_list):
         train_model_1 = i
         print('-------', name[idx])
         train_model_1.fit(train_input, train_output)
         predicted_output = train_model_1.predict(test_input)
+
+        graph = plot_graph(test_output, predicted_output)
+
         print('r_2 statistic: %.2f' % r2_score(test_output, predicted_output))
         print("Mean_absolute_error: %.2f" % mean_absolute_error(test_output, predicted_output))
         print("Mean squared error: %.2f" % mean_squared_error(test_output, predicted_output))
