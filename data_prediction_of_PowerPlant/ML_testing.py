a76e0221d537ca27dd9079d00e760614d7a678a6
atifkarim <atif0902082@gmail.com>
2019-08-18 19:41:39 +0200

    try to call dataframe from another file but failed

--- data_prediction_of_PowerPlant/ML_testing.py
+++ data_prediction_of_PowerPlant/ML_testing.py
@@ -1,4 +1,19 @@
-from main import dataframe_high_correlation
+print('what !!!!')
+from main import dataframe_high_correlation, final_directory
+from model_file import make_dataset, scikit_learn_model
+from sklearn.ensemble import ExtraTreesRegressor
 
+print('AM I HERE NOW !!!!!')
 print(type(dataframe_high_correlation))
 dataframe_high_correlation.head()
+
+print(final_directory)
+
+print('I will be here now')
+train_input, train_output, test_input, test_output = make_dataset(dataframe_high_correlation)
+
+#s_array = dataframe_high_correlation.values
+model_list = [ExtraTreesRegressor()]
+name = ['ExtraTreesRegressor']
+
+model = scikit_learn_model(model_list, name, train_input, train_output, test_input, test_output, final_directory)
